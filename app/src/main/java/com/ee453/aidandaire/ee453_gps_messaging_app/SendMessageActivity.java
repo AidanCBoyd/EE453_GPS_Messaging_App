0952ba1ec180246d454b9422d63899e5291f9b49
Aidan Boyd <a.boyd2@nuigalway.ie>
2018-01-06 15:45:19 +0000

    Deleting unused class implementations

--- app/src/main/java/com/ee453/aidandaire/ee453_gps_messaging_app/SendMessageActivity.java
+++ app/src/main/java/com/ee453/aidandaire/ee453_gps_messaging_app/SendMessageActivity.java
@@ -1,16 +1,12 @@
 package com.ee453.aidandaire.ee453_gps_messaging_app;
 
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.GoogleMap;
 
-public class SendMessageActivity extends AppCompatActivity implements LocationListener {
+public class SendMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +20,5 @@ public class SendMessageActivity extends AppCompatActivity implements LocationLi
         String message = editText.getText().toString();
         intent.putExtra("MESSAGE", message);
         startActivity(intent);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
     }
 }
